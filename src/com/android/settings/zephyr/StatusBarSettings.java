0807b217feea7fb4af33e35e81a499eb297a3509
Roman Birg <roman@cyngn.com>
2014-12-19 21:35:52 +0500

    Settings: add "Show weather" toggle for status bar
    Change-Id: I198481057bb4a0cef618267a48ced18fbd01db33
    
    Conflicts:
    	res/values/custom_strings.xml
    
    Conflicts:
    	src/com/android/settings/broken/StatusBarSettings.java
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/broken_settings_statusbar.xml
    	src/com/android/settings/zephyr/LockScreenSettings.java

--- src/com/android/settings/zephyr/StatusBarSettings.java
+++ src/com/android/settings/zephyr/StatusBarSettings.java
@@ -11,17 +11,26 @@ import android.preference.SwitchPreference;
 import android.preference.PreferenceCategory;
 import android.provider.Settings;
 import android.content.ContentResolver;
+import android.content.res.Resources;
 
 import com.android.internal.logging.MetricsLogger;
 
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
        private static final String ENABLE_TASK_MANAGER = "enable_task_manager";
+       private static final String KEY_LOCK_CLOCK = "lock_clock";
+       private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+
+       private PreferenceScreen mLockClock;
 
        private SwitchPreference mEnableTaskManager;
 
@@ -34,12 +43,20 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.status_bar_settings);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        if (!DeviceUtils.deviceSupportsMobileData(getActivity())) {
+        PreferenceScreen prefSet = getPreferenceScreen(); 
+
+       if (!DeviceUtils.deviceSupportsMobileData(getActivity())) {
             removePreference("status_bar_carrier_label_settings");
         }
  mEnableTaskManager = (SwitchPreference) findPreference(ENABLE_TASK_MANAGER);        
  mEnableTaskManager.setChecked((Settings.System.getInt(resolver,
                 Settings.System.ENABLE_TASK_MANAGER, 0) == 1));
+
+        // mLockClock 
+    	mLockClock = (PreferenceScreen) findPreference(KEY_LOCK_CLOCK);
+        if (!Utils.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
+            prefSet.removePreference(mLockClock);
+        }
     }
 
     @Override
