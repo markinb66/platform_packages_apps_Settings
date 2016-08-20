AOKP/packages_apps_Settings

Fling: Trails width configuration [1/3]
AICPfy:
We use SeekBarPreferenceCham

Change-Id: Ic141363477be7f88db2cd030453766b2fd80e00b

src/com/android/settings/aicp/FlingSettings.java
@@ -41,6 +41,8 @@ import android.preference.PreferenceCategory;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.settings.widget.SeekBarPreferenceCham;
+
 public class FlingSettings extends ActionFragment implements
         Preference.OnPreferenceChangeListener, IconPickHelper.OnPickListener {
     private static final String TAG = FlingSettings.class.getSimpleName();
@@ -57,6 +59,7 @@ public class FlingSettings extends ActionFragment implements
     ColorPickerPreference mLogoColor;
     ColorPickerPreference mRippleColor;
     ColorPickerPreference mTrailsColor;
+    SeekBarPreferenceCham mTrailsWidth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -108,6 +111,12 @@ public class FlingSettings extends ActionFragment implements
         mTrailsColor = (ColorPickerPreference) findPreference("eos_fling_trails_color");
         mTrailsColor.setNewPreviewColor(trailsColor);
         mTrailsColor.setOnPreferenceChangeListener(this);
+
+        mTrailsWidth = (SeekBarPreferenceCham) findPreference("du_fling_trails_width");
+        int width = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.FLING_TRAILS_WIDTH, 15, UserHandle.USER_CURRENT);
+        mTrailsWidth.setValue(width / 1);
+        mTrailsWidth.setOnPreferenceChangeListener(this);
 
         onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.FLING));
     }
@@ -192,6 +201,11 @@ public class FlingSettings extends ActionFragment implements
             int color = ((Integer) newValue).intValue();
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.FLING_TRAILS_COLOR, color);
+            return true;
+        } else if (preference == mTrailsWidth) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.FLING_TRAILS_WIDTH, val * 1, UserHandle.USER_CURRENT);
             return true;
         }
         return false;

markinb66/platform_packages_apps_Settings

src/com/android/settings/custom/nav/FlingSettings.java
@@ -42,6 +42,8 @@ import android.preference.PreferenceCategory;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.settings.chameleonos.SeekBarPreference;
+
 public class FlingSettings extends ActionFragment implements
         Preference.OnPreferenceChangeListener, IconPickHelper.OnPickListener {
     private static final String TAG = FlingSettings.class.getSimpleName();
@@ -58,6 +60,7 @@ public class FlingSettings extends ActionFragment implements
     ColorPickerPreference mLogoColor;
     ColorPickerPreference mRippleColor;
     ColorPickerPreference mTrailsColor;
+    SeekBarPreference mTrailsWidth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -109,6 +112,12 @@ public class FlingSettings extends ActionFragment implements
         mTrailsColor = (ColorPickerPreference) findPreference("eos_fling_trails_color");
         mTrailsColor.setNewPreviewColor(trailsColor);
         mTrailsColor.setOnPreferenceChangeListener(this);
+
+        mTrailsWidth = (SeekBarPreference) findPreference("du_fling_trails_width");
+        int width = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.FLING_TRAILS_WIDTH, 15, UserHandle.USER_CURRENT);
+        mTrailsWidth.setValue(width / 1);
+        mTrailsWidth.setOnPreferenceChangeListener(this);
 
         onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.FLING));
     }
@@ -194,6 +203,11 @@ public class FlingSettings extends ActionFragment implements
             int color = ((Integer) newValue).intValue();
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.FLING_TRAILS_COLOR, color);
+            return true;
+        } else if (preference == mTrailsWidth) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.FLING_TRAILS_WIDTH, val * 1, UserHandle.USER_CURRENT);
             return true;
         }
         return false;